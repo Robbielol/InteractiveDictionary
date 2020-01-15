8397012570e75b6ee251b0386263c5370af73aa9
Robbielol <laffanhome@gmail.com>
2020-01-15 12:19:17 -0600

    First python program. Simple english dictionary.

---
+++ EnglishDictionary.py
@@ -0,0 +1,34 @@
+import json
+from difflib import  get_close_matches
+data = json.load(open("data.json"))
+
+
+def definition(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.title in data:
+        return data[word.title]
+    elif word.upper() in data:  # in case user enters words like USA or NATO
+        return data[word.upper()]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        answer = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
+        if answer == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif answer == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "We didn't understand your entry."
+    else:
+        return "The word doesn't exist. Please double check it."
+
+word = input("Select Word: ")
+
+output = definition(word)
+
+if type(output) == list:
+    for item in output:
+        print(output)
+else:
+    print(output)
+
